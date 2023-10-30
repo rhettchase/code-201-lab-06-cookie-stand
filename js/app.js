66d8fdce231d8de8a8a7964be5c84db7497bbc2a
RHETT CHASE <rhettjchase@gmail.com>
2023-10-30 15:56:07 -0700

    create for loop to generate and store hourly cookie sales

--- js/app.js
+++ js/app.js
@@ -1,24 +1,47 @@
 "use strict";
 
 const seattle = {
+  location: "Seattle",
   minCustomers: 23,
   maxCustomers: 65,
   avgCookies: 6.3,
-  customers: 0, // to be randomly generated
-  hourlyCookies: 0,
+  //   customers: 0, // to be randomly generated
+  hourlyCookies: [],
   generateCustomers: function () {
-    this.customers = random(this.minCustomers, this.maxCustomers);
-    console.log(this.customers);
+    return random(this.minCustomers, this.maxCustomers);
+    // console.log(this.customers);
   },
   generateCookies: function () {
-    this.hourlyCookies = this.avgCookies * this.customers;
-  }
-//   hourlyCookies: this.avgCookies * this.customers,
+    for (let i = 6; i < 19; i++) {
+      this.hourlyCookies.push(
+        Math.floor(this.avgCookies * this.generateCustomers())
+      );
+    }
+  },
+  generateTotalSales: function() {
+    return totalSales(this.hourlyCookies);
+  },
 };
-seattle.generateCustomers();
+// seattle.generateCustomers();
 seattle.generateCookies();
 console.log(seattle);
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function totalSales(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+// function hourlySales() {
+//   for (let i = 6; i < 19; i++) {
+//     let cookies = this.hourlyCookies.push(
+//       Math.floor(this.avgCookies * this.generateCustomers())
+//     );
+//     this.hourlyCookies.push(cookies);
+//   }
+// }
